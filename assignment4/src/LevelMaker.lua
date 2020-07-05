5ee19568d74e92c245d08af6146faa81eddcfd5e
jlangdesign <samusaran2008@gmail.com>
2020-07-05 19:44:36 -0400

    Ensured that first column is never chasm

--- assignment4/src/LevelMaker.lua
+++ assignment4/src/LevelMaker.lua
@@ -16,7 +16,7 @@ function LevelMaker.generate(width, height)
     local objects = {}
 
     local tileID = TILE_ID_GROUND
-    
+
     -- whether we should draw our tiles with toppers
     local topper = true
     local tileset = math.random(20)
@@ -30,15 +30,16 @@ function LevelMaker.generate(width, height)
     -- column by column generation instead of row; sometimes better for platformers
     for x = 1, width do
         local tileID = TILE_ID_EMPTY
-        
+
         -- lay out the empty space
         for y = 1, 6 do
             table.insert(tiles[y],
                 Tile(x, y, tileID, nil, tileset, topperset))
         end
 
-        -- chance to just be emptiness
-        if math.random(7) == 1 then
+        -- chance to just be emptiness in any column other than the first
+        -- (first column must be solid so player can spawn on it)
+        if math.random(7) == 1 and x ~= 1 then
             for y = 7, height do
                 table.insert(tiles[y],
                     Tile(x, y, tileID, nil, tileset, topperset))
@@ -56,7 +57,7 @@ function LevelMaker.generate(width, height)
             -- chance to generate a pillar
             if math.random(8) == 1 then
                 blockHeight = 2
-                
+
                 -- chance to generate bush on pillar
                 if math.random(8) == 1 then
                     table.insert(objects,
@@ -66,18 +67,18 @@ function LevelMaker.generate(width, height)
                             y = (4 - 1) * TILE_SIZE,
                             width = 16,
                             height = 16,
-                            
+
                             -- select random frame from bush_ids whitelist, then random row for variance
                             frame = BUSH_IDS[math.random(#BUSH_IDS)] + (math.random(4) - 1) * 7
                         }
                     )
                 end
-                
+
                 -- pillar tiles
                 tiles[5][x] = Tile(x, 5, tileID, topper, tileset, topperset)
                 tiles[6][x] = Tile(x, 6, tileID, nil, tileset, topperset)
                 tiles[7][x].topper = nil
-            
+
             -- chance to generate bushes
             elseif math.random(8) == 1 then
                 table.insert(objects,
@@ -138,7 +139,7 @@ function LevelMaker.generate(width, height)
                                             player.score = player.score + 100
                                         end
                                     }
-                                    
+
                                     -- make the gem move up from the block and play a sound
                                     Timer.tween(0.1, {
                                         [gem] = {y = (blockHeight - 2) * TILE_SIZE}
@@ -161,6 +162,6 @@ function LevelMaker.generate(width, height)
 
     local map = TileMap(width, height)
     map.tiles = tiles
-    
+
     return GameLevel(entities, objects, map)
 end
